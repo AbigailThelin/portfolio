69016d2b4332954a5de89c89cb94f0bc06ed22f9
Abigail Thelin <abbyjthelin@gmail.com>
2017-09-06 16:13:12 -0600

    hamburger menu

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import menu from './../assests/align-justify.svg'
 
+
 export default class App extends Component {
 
 	constructor(){
@@ -9,31 +10,27 @@ export default class App extends Component {
 		this.state={
 
 		}
-		this.toggle = this.toggle.bind(this)
 	}
-
-		toggle(){
-			
-		}
 
     
 	render(){
 		return (
 			<div className='App'>
-				<img onClick={this.toggle} className="threeLines" src={menu} alt="menu"/> 
-				<div className="menu">
+				 <img className="threeLines" src={menu} alt="menu"/>
+				 <div className="menu">
 					<ul>
 						<li>ABOUT</li>
 						<li>PROJECTS</li>
 						<li>CONTACT</li>
 					</ul>
-				</div>
+				</div> 
 
-                <div className="front_text">
-                    {/* <p className="name">ABIGAIL,</p>
-                    <p className="job">WEB DEV</p> */}
-                </div>
+	
+                {/* <div className="front_text">
+                     <p className="name">ABIGAIL,</p>
+                    <p className="job">WEB DEV</p> 
+                </div> */}
 			</div>
 		)
 	}
-}
+} 
