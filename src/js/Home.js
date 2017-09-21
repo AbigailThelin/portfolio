2f8a7f30f27756e88425f577843711db7272e31a
Abigail Thelin <abbyjthelin@gmail.com>
2017-09-21 14:23:47 -0600

    toggle Menu

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import menu from './../assests/align-justify.svg'
-import me from '../assests/IMG_5070.svg'
+// import me from '../assests/IMG_5070.svg'
 import x from '../assests/X.svg'
 // import { slide as Menu } from 'react-burger-menu'
 
@@ -38,28 +38,26 @@ export default class App extends Component {
 				this.state.showModal
 				?
 				<div className="menu">
-					<menu>
-						<img src={x} alt=""/>
-						<a id="home" className="menu-item" href="/">Home</a>
-						<a id="about" className="menu-item" href="/about">About</a>
-						<a id="contact" className="menu-item" href="/contact">Contact</a>
-						<a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+					<menu className="menu">
+						<img onClick={this.exit} className="x" src={x} alt=""/>
+						<a id="li_items" className="home" href="/">Home</a>
+						<a id="li_items" className="about" href="/about">About</a>
+						<a id="li_items" className="contact" href="/contact">Contact</a>
+						<a id="li_items" className="settings" href="/settings">Settings</a>
 					</menu>
 				</div> 
 				:
 				null
 				}
-
 				<img onClick={this.showSettings} className="threeLines" src={menu} alt="menu"/>
-                <div className="front_text">
-                     <p className="name">a</p>
+				
 
-				<div className="rightSide"></div>
-				<div className="rightSideTop">
-					<img className="mee" src={me} alt="me"/>
+				<div className="front-text">
+					<p className="name">YOOO</p>
 				</div>
-                </div> 
-			</div>
+
+
+				</div>
 		)
 	}
 } 
